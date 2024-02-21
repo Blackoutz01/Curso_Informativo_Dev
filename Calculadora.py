bbd064cb81bc50015439faca985214e75c70c4b2
Blackoutz01 <160240139+Blackoutz01@users.noreply.github.com>
2024-02-21 11:57:46 -0300

    Create Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,14 @@
+print("Selecione a operação desejada")
+print("1.Adição")
+print("2.Subtração")
+print("3.Multiplicação")
+print("4.Divisão")
+
+opcao = input("Digite o número da operação desejada (1/2/3/4):")
+
+numero1 = float(input("Digite o primeiro número:"))
+numero2 = float(input("Digite o segundo número"))
+.
+
+
+
